eefdb53b210250af0a0343d7b67fed3d7d0fefa2
quocghiendev <kieukienquoc2006@gmail.com>
2025-09-17 16:52:50 +0700

    Add files via upload

---
+++ Trungbinhsochuso.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+int main() {
+    system("cls");
+    // INPUT - @STUDENT: ADD YOUR CODE FOR INPUT HERE:
+	int i1,i2,k,tong=0,dem=0;
+	scanf("%d%d",&i1,&i2);
+    
+
+    // Fixed Do not edit anything here.
+    printf("\nOUTPUT:\n");
+    //@STUDENT: WRITE YOUR OUTPUT HERE
+    if(i1>=i2){
+		for(k=i2;k<=i1;k++){
+			tong=tong+k;
+			dem=dem+1;
+		}
+	}else{
+		for(k=i1;k<=i2;k++){
+			tong=tong+k;
+			dem=dem+1;
+		}	
+	}
+	printf("%.1f",(float)tong/dem);
+    //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+    printf("\n");
+    system("pause");
+    return(0);
+}
